283fd1345767de0160da47531b292582f4a60f14
kallekj <kalle@djervbrant.net>
2020-07-17 08:54:54 +0100

    Small change

--- autodeploy.py
+++ autodeploy.py
@@ -23,7 +23,7 @@ def deploy(js):
     p = config[js["repository"]["name"]]
     subprocess.run(["git", "-C", "/home/pi/Discord-bot-ture/", "pull"], cwd=p["path"])
     #subprocess.run(["systemctl", "restart", p["service"]])
-    commit = get_commit(check_output(["git", "log", "-1", "--oneline"]))
+    commit = get_commit(check_output(["git", "-C", "/home/pi/Discord-bot-ture/", "log", "-1", "--oneline"]))
     subprocess.run(["python3", "/home/pi/Discord-bot-ture/bot.py", "boken123", commit])
 
 
